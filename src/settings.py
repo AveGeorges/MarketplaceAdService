88596350e6febf1843dda47965524a2153f949ab
AveGeorge <smoly1995@gmail.com>
2026-08-10 23:28:01 +0300

    fix: lint settings

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,10 @@
-from pydantic import Field, model_validator
+from pydantic import model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", 
-        env_file_encoding="utf-8",
-        extra="ignore" 
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
     database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
@@ -22,7 +20,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int | None = None
     POSTGRES_USERNAME: str | None = None
     POSTGRES_PASSWORD: str | None = None
-    
+
     KAFKA_BROKERS: str | None = None
     KAFKA_TOPIC_MARKETPLACE_ADS: str | None = None
 
@@ -33,16 +31,17 @@ class Settings(BaseSettings):
             if url.startswith("postgres://"):
                 url = url.replace("postgres://", "postgresql+asyncpg://", 1)
             self.database_url = url
-            
+
         elif self.POSTGRES_HOST and self.POSTGRES_USERNAME:
             self.database_url = (
                 f"postgresql+asyncpg://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}"
-                f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT or 5432}/{self.POSTGRES_DATABASE_NAME}"
+                f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT or 5432}/"
+                f"{self.POSTGRES_DATABASE_NAME}"
             )
 
         if self.KAFKA_BROKERS:
             self.kafka_bootstrap_servers = self.KAFKA_BROKERS
-            
+
         if self.KAFKA_TOPIC_MARKETPLACE_ADS:
             self.kafka_topic_ads = self.KAFKA_TOPIC_MARKETPLACE_ADS
 
